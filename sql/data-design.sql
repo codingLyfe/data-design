2b2c2dad880ed1795fd504d711f4de4e68f50a2a
Tristan Bennett <tmbennett2011@msn.com>
2018-01-17 11:37:20 -0700

    inserted into profile and article

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -43,7 +43,19 @@ INSERT INTO profile (profileId, profileActivationToken, profileEmail, profileHas
 VALUES (UNHEX(REPLACE('a544cd25-710c-4a16-839c-b32f942e1539','-','')), '1', 'markLawson', '1a2b', 'Mark', '3c4d');
 
 SELECT profileEmail
-FROM profile;
+FROM profile
+WHERE profileId = UNHEX(REPLACE('a544cd25-710c-4a16-839c-b32f942e1539','-',''));
+
+UPDATE profile
+SET profileHash = '33f7b2768dc230805ea073943708a02d4e4c70262ea2b7fb1c343aaeacb447447815fd78476976091a69d3f72ebe15402a5c9bbab0b11d758439eeff11376916',
+	profileSalt = 'bdcd659f20e1b25d7cdf297256c8a6fa934829cf733496e75c9c83ac43f5b50d'
+WHERE profileId = UNHEX(REPLACE('a544cd25-710c-4a16-839c-b32f942e1539','-',''));
+
+SELECT profileHash
+FROM profile
+WHERE profileId = UNHEX(REPLACE('a544cd25-710c-4a16-839c-b32f942e1539','-',''));
+
+
 
 INSERT INTO article (articleId, articleProfileId, articleContent, articleDateTime, articleTitle)
-	VALUES (unhex(replace('63b9c8c9-9151-41fb-8213-0ffffe933c0a','-','')), 'mark1', 'Lawson and Sons', DATE (), 'L or S');
+	VALUES (unhex(replace('63b9c8c9-9151-41fb-8213-0ffffe933c0a','-','')), UNHEX(REPLACE('a544cd25-710c-4a16-839c-b32f942e1539','-','')), 'Lawson and Sons', '2018-01-17 10:52:30.651024', 'L or S');
