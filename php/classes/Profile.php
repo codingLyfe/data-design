8844776f1ac4194f3777991153d69401632dc241
Tristan Bennett <tmbennett2011@msn.com>
2018-01-21 08:11:46 -0700

    added doc block

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,5 +1,36 @@
 <?php
 
+namespace Edu\Cnm\DataDesign;
+
+
+//getting undefined namespace error
+//use Ramsey\Uuid\Uuid;
+
+/**
+ * Cross section of a "Medium" profile
+ *
+ * This Profile can be considered an example of what might be stored in a User's Profile on Medium.
+ *
+ * @author Tristan Bennett tbennett19@cnm.edu
+ * @version 4.0.0
+ * @package Edu\Cnm\DataDesign
+ */
+
+
+
+
 class Profile {
+
+		private $profileId;
+
+		private $profileName;
+
+		private $profileActivationToken;
+
+		private $profileEmail;
+
+		private $profileHash;
+
+		private $profileSalt;
 
 }
